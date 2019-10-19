54b4ab32134cf32a256398011b0d367d18fc4d7e
Addy <aclaure@gmail.com>
2019-10-19 09:44:09 -0400

    error handling

--- src/Bootstrap.php
+++ src/Bootstrap.php
@@ -1,3 +1,28 @@
 <?php declare(strict_types = 1);
 
-echo 'Hello World!';
+namespace Example;
+
+require __DIR__ . '/../vendor/autoload.php';
+
+error_reporting(E_ALL);
+
+$environment = 'development';
+
+/**
+* Register the error handler
+*/
+$whoops = new \Whoops\Run;
+if ($environment !== 'production') 
+{
+    $whoops->pushHandler(new \Whoops\Handler\PrettyPageHandler);
+} else 
+{
+    $whoops->pushHandler(
+        function($e)
+        {
+            echo 'Todo: Friendly error page and send an email to the developer';
+        });
+}
+$whoops->register();
+
+throw new \Exception;
